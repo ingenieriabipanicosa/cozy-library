5251b1bd0ff9396960c5afe52be912123096b4d8
ingenieriabipanicosa <likykizz@gmail.com>
2026-07-09 11:08:52 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # Configuración de la página (estilo amplio)
-st.set_page_config(page_title="Mi Bitácora Aesthetic", layout="wide")
+st.set_page_config(page_title="Bipanick Engineer's Booklist ", layout="wide")
 
 # Título de bienvenida
-st.markdown("<h1 style='text-align: center; color: #FF69B4;'>✨ Mi Bitácora Mágica ✨</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #F48FB1;'>Bipanick Engineer's Booklist </h1>", unsafe_allow_html=True)
 
 # Crear 3 columnas para los botones de inicio
 col1, col2, col3 = st.columns(3)
@@ -29,5 +29,5 @@ if 'pagina' not in st.session_state:
     st.session_state.pagina = "HOME"
 
 if st.session_state.pagina == "BL":
-    st.write("### 💖 Sección de BL")
+    st.write("### 💖 BL'S LIST")
     st.write("Aquí irán tus listas de lectura...")
